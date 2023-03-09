bb9a2643dee939d5b4b41225c72272a8da4c98dd
pymike00 <pymike00@gmail.com>
2023-03-09 22:25:40 +0100

    UPDATE: converter.py

--- converter.py
+++ converter.py
@@ -4,7 +4,7 @@ import torch.onnx
 from model import U2NET
 
 torch_model = U2NET(3,1)
-model_path = "u2net_bce_itr_2000_train_2.445896_tar_0.330302.pth"
+model_path = "to-convert.pth"
 batch_size = 1
 
 torch_model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
@@ -13,4 +13,4 @@ torch_model.eval()
 x = torch.randn(batch_size, 3, 320, 320, requires_grad=True)
 torch_out = torch_model(x)
 
-torch.onnx.export(torch_model, x, "new-model.onnx", export_params=True, opset_version=11, do_constant_folding=True, input_names = ['input'], output_names = ['output'], dynamic_axes = {'input' : {0: 'batch_size'}, 'output': {0: 'batch_size'}})
+torch.onnx.export(torch_model, x, "u2net.onnx", export_params=True, opset_version=11, do_constant_folding=True, input_names = ['input'], output_names = ['output'], dynamic_axes = {'input' : {0: 'batch_size'}, 'output': {0: 'batch_size'}})
