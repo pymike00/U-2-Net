6388048d3cfb3da42cee51ce5f779e3acae6de4f
pymike00 <pymike00@gmail.com>
2023-03-09 22:26:36 +0100

    UPDATE: converter.py

--- converter.py
+++ converter.py
@@ -7,7 +7,7 @@ torch_model = U2NET(3,1)
 model_path = "to-convert.pth"
 batch_size = 1
 
-torch_model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
+torch_model.load_state_dict(torch.load(model_path)) # , map_location=torch.device("cpu")
 torch_model.eval()
 
 x = torch.randn(batch_size, 3, 320, 320, requires_grad=True)
